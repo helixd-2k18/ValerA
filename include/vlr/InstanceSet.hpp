18049e2db808c531ab7d07e256c0662a557dbef0
helixd-2k18 <helixd.2k18@gmail.com>
2020-07-21 04:53:20 +0000

    instance set

--- include/vlr/InstanceSet.hpp
+++ include/vlr/InstanceSet.hpp
@@ -1,17 +1,18 @@
 #pragma once
 #include "./Config.hpp"
+#include "./SetBase.hpp"
 
 namespace vlr {
 
-    class InstanceSet : public std::enable_shared_from_this<InstanceSet> { protected: 
-    public: 
-        InstanceSet() { this->constructor(); };
-        InstanceSet(vkt::uni_ptr<Driver> driver) { this->constructor(driver); };
+    class InstanceSet : public SetBase_T<vkh::VsGeometryInstance> { protected: 
+        vkt::uni_ptr<Driver> driver = {};
 
-        virtual void constructor() {};
-        virtual void constructor(vkt::uni_ptr<Driver> driver) {
-            
-        };
+    public: 
+        InstanceSet() : SetBase_T<vkh::VsGeometryInstance>() { this->constructorExtend0(); };
+        InstanceSet(vkt::uni_ptr<Driver> driver, vkt::uni_arg<DataSetCreateInfo> info) : SetBase_T<vkh::VsGeometryInstance>(driver, info) { this->constructorExtend0(driver); };
+
+        virtual void constructorExtend0() {};
+        virtual void constructorExtend0(vkt::uni_ptr<Driver> driver) {};
     };
 
 };
